8963932a892989bcdf3bf4cf14521aaa04cc60ac
AscendoHack <52598832+AscendoHack@users.noreply.github.com>
2019-07-06 18:14:15 +0700

    Baru rilis

---
+++ out.py
@@ -0,0 +1,161 @@
+#!/bin/bash/python
+import random
+import sys
+import time
+import os
+os.system('clear')
+def entup(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush(s)
+        time.sleep(30. /500)
+
+print "\033[1;32m=====  ==    = ======= =     = ======"
+print "\033[1;32m==     = =   =    =    =     = =    ="
+print "\033[1;32m=====  =  =  =    =    =     = ======"
+print "\033[1;32m==     =   = =    =    =     = ="
+print "\033[1;32m=====  =    ==    =    ======= ="
+os.system ('date')
+
+print "\033[1;31m============================="
+print "\033[1;31m=        Menu Hacking       ="
+print "\033[1;31m============================="
+print "\033[1;31m=[1] Dark Fb V 1.6          ="
+print "\033[1;31m=[2] Dark Fb V 1.7          ="
+print "\033[1;31m=[3] Sadap Whatsapp         ="
+print "\033[1;31m=[4] Spam Telkomsel         ="
+print "\033[1;31m=[5] Spam Indosat           ="
+print "\033[1;31m=[6] Hammer                 ="
+print "\033[1;31m=[7] Spam Call              ="
+print "\033[1;31m=[8] Sadap Metasploit New   ="
+print "\033[1;31m=[9] Tembak Kuota All       ="
+print "\033[1;31m=[10] Diamond Ilegal Ml     ="
+print "\033[1;31m=[11] Diamond Ilegal Ff     ="
+print "\033[1;31m=[12] Kirim Virus Lwt Nomor ="
+print "\033[1;31m=[13] Auto Follow Ig        ="
+print "\033[1;31m=[14] Auto Like Ig          ="
+print "\033[1;31m=[15] Exit                  ="
+print "\033[1;31m============================="
+
+pilih = raw_input("Pilih Nomor :")
+
+if pilih == '1':
+   print "\033[1;38mMOHON LOGIN DULU"
+   pilih == raw_input("[+]Username :")
+   pilih == raw_input("[+]Password :")
+   print "[!]SEDANG LOGIN..."
+   time.sleep(10)
+   print "[!]LOGIN GAGAL..."
+   time.sleep(10)
+   print "\033[1;32mMOHON MAAF TOOLS INI KAMI TUTUP KARENA DISALAH GUNAKAN!!!"
+   print "COBALAH TOOLS YG LAIN"
+   os.system('python2 out.py')
+elif pilih == '2':
+   print "\033[1;38m[!]MOHON LOGIN LECESSE"
+   pilih == raw_input("USERNAME :")
+   pilih == raw_input("PASSWORD :")
+   print "LOGIN GAGAL..."
+   time.sleep(12)
+   os.system('python2 out.py')
+elif pilih == '3':
+   print "\033[1;34m=============================="
+   print "=  ANDA YG TANGGUNG JAWAB YA ="
+   print "=  SEDANG BERJALAN KE TOOLS  ="
+   print "=       PROGRAM ILEGAL       ="
+   print "=============================="
+   time.sleep(10)
+   print "\033[1;38m[+]MASUKAN NOMOR ANDA"
+   pilih == raw_input("MASUKAN NOMOR ANDA :")
+   time.sleep(16)
+   print "\033[1;35m[+]MASUKAN NOMOR KORBAN"
+   pilih == raw_input("MASUKAN NOMOR KORBAN :")
+   time.sleep(16)
+   print "SUKSES GUNAKAN KODE 067124"
+   pos.system('python2 out.py')
+elif pilih == '4':
+   print "\033[1;36m[PROGRAM BY ENTUP]"
+   pilih == raw_input("MASUKAN NOMOR TARGET :")
+   print "MOHON TUNGGU..."
+   time.sleep(80)
+   print "SUKSES"
+   time.sleep(10)
+   os.system('python2 out.py')
+elif pilih == '5':
+   print "\033[1;36mPROGRAM BY ENTUP"
+   time.sleep(10)
+   print "MASUKAN NOMOR KORBAN"
+   pilih == raw_input("MASUKAN NOMOR KORBAN COK :")
+   time.sleep(10)
+   print"SUKSES"
+   os.system('python2 out.py')
+elif pilih == '6':
+   print "\033[1;34mTOOLS SEDANG DIPERBAIKI"
+   time.sleep(10)
+   print "SILAHKAN PILIH YG LAIN"
+   os.system('python2 out.py')
+elif pilih == '7':
+   print "PROGRAM BY ENTUP"
+   time.sleep(18)
+   pilih == raw_input("MASUKAN NOMOR KORBAN :")
+   time.sleep(100)
+   print "SUKSES"
+   os.system('python2 out.py')
+elif pilih == '8':
+   print "TOLS DALAM TAHAP PERBAIKAN!!!"
+   os.system('python2 out.py')
+elif pilih == '9':
+   pilih == raw_input("liceense :")
+   print "LOGIN GAGAL.."
+   os.system('python2 out.py')
+elif pilih == '10':
+   print "MASUKAN ID ANDA"
+   pilih == raw_input("MASUKAN ID :")
+   time.sleep(5)
+   pilih == raw_input("MASUKAN JUMLAH :")
+   print "AKUN ANDA AKAN DI BANNED JADI SIAPKAN AKUN TUMBAL"
+   time.sleep(5)
+   pilih == raw_input("ANDA SUDAH MENERIMA RESIKO NYA BLOM y/n :")
+   print "SUKSES"
+elif pilih == '11':
+   print "MASUKAN ID "
+   pilih == raw_input("MASUKAN ID FF :")
+   print "MOHON TUNGGU..."
+   time.sleep(30)
+   print "SUKSES"
+   os.system('python2 out.py')
+elif pilih == '12':
+   print "MEMBUAT VIRUS MEMATIKAN"
+   time.sleep(100)
+   print "VIRUS SUKSES DIBUAT"
+   time.sleep(15)
+   print "MASUKAN NOMOR TELEPHON TARGET"
+   pilih == raw_input("MASUKAN NOMOR TARGET :")
+   time.sleep(2)
+   print "MOHON TUNGGU PROSES SANGAT LAMA"
+   time.sleep(100)
+   print "SUKSES"
+   os.system('python2 out.py')
+elif pilih == '13':
+   print "TOOLS AUTO FOLLOW IG"
+   pilih == raw_input("USERNAME :")
+   pilih == raw_input("PASSWORD :")
+   time.sleep(5)
+   print "LOGIN SUKSES"
+   pilih == raw_input("MASUKAN JUMLAH :")
+   time.sleep(50)
+   print "SUKSES DIKIRIM"
+   os.system('python2 out.py')
+elif pilih == '14':
+   print "MASUKAN URL ANDA :"
+   time.sleep(5)
+   pilih == raw_input("MASUKAN URL :")
+   time.sleep(10)
+   print "SUKSES"
+   os.system('python2 out.pu')
+elif pilih == '15':
+   print "THANKS SUDAH MAMPIR HEHE"
+   os.system('exit')
+else:
+    print "Mohon maaf pilihan anda salah"
+    time.sleep(2)
+    os.system('python2 out.py')
